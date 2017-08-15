1038c0b97b5898a56fc9949b997559039f8695d5
August Møbius <am_94@live.dk>
2017-08-15 12:09:01 +0200

    Adds new classbox

---
+++ 02350Demo/Command/ResizeItemCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02350Demo.Model;
+
+namespace _02350Demo.Command
+{
+    class ResizeItemCommand : IUndoRedoCommand
+    {
+        // Classboxes can only be resized horizontally, hence only left and right controls - Vertical resize in controlled with content of box
+        private ClassBox classBox;
+        private double offsetLeft;
+        private double offsetRight;
+
+        public ResizeItemCommand(ClassBox _classBox, double _offsetLeft, double _offsetRight)
+        {
+            classBox = _classBox;
+            offsetLeft = _offsetLeft;
+            offsetRight = _offsetRight;
+
+        }
+
+        public void Execute()
+        {
+            if (offsetLeft != 0.0)
+            {
+                classBox.CanvasCenterX += - offsetLeft / 2;
+                classBox.Width += offsetLeft;
+            }
+            else
+            {
+                classBox.CanvasCenterX += offsetRight / 2;
+                classBox.Width += offsetRight;
+            }
+        }
+
+        public void UnExecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
