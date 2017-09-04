3933f2e84a49dcf02685f84a72852e9deb6a38a9
August Møbius <am_94@live.dk>
2017-09-05 01:03:24 +0200

    EdgeViewModel

--- 02350Demo/ViewModel/EdgeViewModel.cs
+++ 02350Demo/ViewModel/EdgeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using GalaSoft.MvvmLight;
 using _02350Demo.Model;
 
@@ -11,9 +12,25 @@ namespace _02350Demo.ViewModel
     class EdgeViewModel : ViewModelBase
     {
         public Edge edge { get; }
+    
+        public bool isSelected { get; set; }
+
+        public ClassBox Source
+        {
+            get { return edge.Source; }
+            set { edge.Source = value; RaisePropertyChanged(() => Source); }
+        }
+
+        public ClassBox Sink
+        {
+            get { return edge.Source; }
+            set { edge.Sink = value; RaisePropertyChanged(() => Sink); }
+        }
+
 
         public EdgeViewModel()
         {
+
         }
     }
 }
